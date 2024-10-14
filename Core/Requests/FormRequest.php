aa15b89a2bf3ee0d7c516251d4e39f239c0c1eb0
Favour Oladeji <oladejifavour100@gmail.com>
2024-10-14 16:26:01 +0100

    feature: add authorization login

--- Core/Requests/FormRequest.php
+++ Core/Requests/FormRequest.php
@@ -2,6 +2,8 @@
 
 namespace  Core\Requests;
 
+use Core\Validator;
+
 class FormRequest extends Request implements FormRequestInterface
 {
     public $errors;
@@ -9,6 +11,46 @@ class FormRequest extends Request implements FormRequestInterface
     public function authorize()
     {
         return true;
+    }
+
+    protected function rules()
+    {
+        return [];
+    }
+
+    protected function attributes()
+    {
+        return [];
+    }
+
+    public function validate($method)
+    {
+        $requestRules = $this->rules();
+        foreach ($requestRules as $key => $rules) {
+            if (strtoupper($method) == 'POST')
+            {
+                $value = $this->post($key);
+                foreach ($rules as $rule)
+                {
+                    if (!method_exists(Validator::class, $rule))
+                    {
+                        throw new \Exception("The validation rule '$rule' does not exist");
+                    }
+                    $validationResult = call_user_func_array([Validator::class, $rule], [$value]);
+                    if (!$validationResult['success'])
+                    {
+                        $replace = $key;
+                        if (array_key_exists($key, $this->attributes()))
+                        {
+                            $replace = $this->attributes()[$key];
+                        }
+                        $message = str_replace('{key}', $replace, $validationResult['message']);
+                        $this->errors[$key][] = $message;
+                    }
+                    
+                }
+            }
+        }
     }
 
 }
