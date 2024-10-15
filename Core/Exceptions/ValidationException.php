69ffd946d5dddfe9d36698c9c311ef3b35a5a678
Favour Oladeji <oladejifavour100@gmail.com>
2024-10-15 14:38:24 +0100

    feature: a better way of storing old values whenever the validation exception is being called

--- Core/Exceptions/ValidationException.php
+++ Core/Exceptions/ValidationException.php
@@ -2,19 +2,40 @@
 
 namespace Core\Exceptions;
 
+use Core\Requests\Request;
 use Core\Session;
 
 class ValidationException extends \Exception{
 
     public readonly array $errors;
-    public readonly array $old;
-    public static function throw($errors, $old)
+
+    public static function throw(?array $errors)
     {
+        
         $instance =  new static('The form failed to validate');
-        $instance->errors = $errors;
-        $instance->old = $old;
-        Session::flash('old', $old);
-        Session::flash('errors', $errors);
+        $instance->errors = static::transformErrors($errors);
+        static::setOldValues();
+        Session::flash('errors', $instance->errors);
         throw $instance;
     }
+
+    protected static function setOldValues()
+    {
+        Session::flash('old', Request::attributes());
+    }
+
+    protected static function transformErrors($errors)
+    {
+        foreach ($errors as $key => $errorMessages)
+        {
+            if (!is_array($errorMessages))
+            {
+                $errors[$key] = [$errorMessages];
+            }
+        }
+        
+        return $errors;
+
+    }
+
 }
