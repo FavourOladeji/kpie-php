01b3b630b77f218917e58424b9ee62d66f8bae16
Favour Oladeji <oladejifavour100@gmail.com>
2024-10-15 10:30:06 +0100

    feature: create session class

---
+++ Core/Session.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Core;
+
+class Session{
+    public static function flash($key, $value)
+    {
+        $_SESSION['_flash'][$key] = $value;
+    }
+
+    public static function unflash()
+    {
+        unset($_SESSION['_flash']);
+    }
+
+    public static function get($key, $default = null)
+    {
+        return $_SESSION['_flash'][$key] ?? $default;
+    }
+}
