90a3396ee2e28264d1c6b34cca3dc11fcf12a0df
Favour Oladeji <oladejifavour100@gmail.com>
2024-10-15 10:52:23 +0100

    implement session in the application

--- public/index.php
+++ public/index.php
@@ -1,14 +1,14 @@
 <?php
+use Core\Session;
 
 
 const BASE_PATH = __DIR__ . '/../';
 
 require BASE_PATH . 'vendor/autoload.php';
 
+session_start();
 $appUrl = trim(config('app.url'), '/');
 define('APP_URL',$appUrl);
 
 require_once base_path('bootstrap.php');
-
-
-dd($_SERVER);
+Session::unflash();
